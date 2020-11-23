972729b7c358d49795ba488e378c68f701ab4120
AzzielzaST <73247813+AzzielzaST@users.noreply.github.com>
2020-11-23 22:19:33 +0700

    Add files via upload

---
+++ Scenes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    // pindah scene ke menu
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    // pindah scene ke loading
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+    // keluar dari gamr 
+    public void KeluarScene()
+    {
+        Application.Quit();
+    }
+}
